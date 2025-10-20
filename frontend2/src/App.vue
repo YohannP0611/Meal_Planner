10e9112d69048f5275cb8aa006e9bb588371ef53
Mayomat <matthieu.sirier@efrei.net>
2025-10-21 00:19:52 +0200

    Redo the commit because last failed

---
+++ frontend2/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Logo from './assets/MPlogo.png'
+</script>
+
+<template>
+  
+  <div id="HeaderContainer">
+    <div id="TopLeftProject">
+      <img :src=Logo alt="MealPlanner Logo" id="BarLogo"/>
+      <h1>Meal Planner</h1>
+    </div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/meals">Meals</router-link>
+    </nav>
+  </div>
+  <router-view/>
+</template>
