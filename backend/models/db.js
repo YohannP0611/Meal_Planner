9b83bdd815657c00f09f4dd1dd17dac3ff3aa8b9
Pouillieute <91455596+YohannP0611@users.noreply.github.com>
2025-11-28 02:34:57 +0100

    changed db.js information to use .env

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "SQLBizar2022!",
-  database: process.env.DB_NAME || "meal_planner",
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE ,
+  port: process.env.DB_PORT 
 });
 
 db.connect((err) => {
