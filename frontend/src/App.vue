fff2f0342bf8b86271c9f77861f9754aa4a54226
Pouillieute <91455596+YohannP0611@users.noreply.github.com>
2025-12-11 19:09:19 +0100

    improvements

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,15 +19,28 @@ provide('mealPlans', mealPlans)
       <h1>Meal Planner</h1>
     </div>
 
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/meals">Meals</router-link> |
-      <router-link to="/ingredients">Ingredients</router-link> |
-      <router-link to="/recipes">Recipes</router-link> |
-      <router-link to="/shops">Shops</router-link> |
-      <router-link to="/login" class="auth-link"><span class="account-icon">👤</span><span>Sign in / Log in</span></router-link> |
+    <!-- center navigation -->
+    <nav id="MainNav">
+      <router-link to="/">Home</router-link>
+      <span class="nav-separator">|</span>
+      <router-link to="/meals">Meals</router-link>
+      <span class="nav-separator">|</span>
+      <router-link to="/ingredients">Ingredients</router-link>
+      <span class="nav-separator">|</span>
+      <router-link to="/recipes">Recipes</router-link>
+      <span class="nav-separator">|</span>
+      <router-link to="/shops">Shops</router-link>
     </nav>
+
+    <!-- right side: sign in / log in -->
+    <div id="AuthNav">
+      <router-link to="/login" class="auth-link">
+        <span class="account-icon">👤</span>
+        <span>Sign in / Log in</span>
+      </router-link>
+    </div>
   </div>
 
   <router-view />
 </template>
+
